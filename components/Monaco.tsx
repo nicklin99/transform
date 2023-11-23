46bea363fe07eca2989321fe14b02bb1dda7484b
nicklin99 <authen@qq.com>
2023-11-23 14:46:14 +0800

    配置中文

--- components/Monaco.tsx
+++ components/Monaco.tsx
@@ -1,6 +1,12 @@
 import React from "react";
-import Editor from "@monaco-editor/react";
+import Editor, { loader } from "@monaco-editor/react";
 import { Pane, Spinner } from "evergreen-ui";
+
+loader.config({
+  paths: {
+    vs: "https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.25.0/min/vs"
+  }
+});
 
 export function processSize(size) {
   return !/^\d+$/.test(size) ? size : `${size}px`;
